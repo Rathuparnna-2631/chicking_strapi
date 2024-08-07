3868dd8ac86d607d7893306fd1d2fb6908a0280b
rathuparnna <rathuparnna.pc@emvigotech.com>
2024-08-07 14:30:08 +0530

    graphql  integration

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -2,13 +2,13 @@ const { initializeApp } = require("firebase/app");
 const { getFirestore } = require("firebase/firestore");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8-D9KGNSeHUl9tmWXH3H12bs42_r2o5s",
-  authDomain: "strapi-chicking.firebaseapp.com",
-  projectId: "strapi-chicking",
-  storageBucket: "strapi-chicking.appspot.com",
-  messagingSenderId: "245771241220",
-  appId: "1:245771241220:web:f9b8e3fa982843bb0e3251"
-};
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+  };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
